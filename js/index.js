ce9cc8d4d66e1755c66b3967a1e8de83978c0f5c
MarcosTorreiro <MarcosTorreiro@users.noreply.github.com>
2017-10-21 20:47:50 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,77 @@
+    $(document).ready(function(){
+
+      new WOW().init();
+    
+      $('#banner').css({'height':$(window).height()+'px'});
+      var flag = false;
+      var scroll;
+      $(window).scroll(function(){
+        scroll = $(window).scrollTop();
+        if(scroll>200){
+          if(!flag){
+            $('#logo').css({'margin-top':'-14px','width':'55px','height':'55px'});
+            $('header').css({'background-color':'#1f1f1f','color':'grey'});
+            $('nav a').css({'color':'white'});
+            $('nav a').hover(function(){
+              $(this).css({'color':'#86EFCC'});
+            },function(){
+              $(this).css({'color':'white'});
+            });
+            flag = true;
+          }
+        }else{
+          if(flag){
+            $('#logo').css({'margin-top':'150px','width':'250px','height':'250px'});
+            $('header').css({'background-color':'transparent', 'color':'yellow'});
+            $('nav a').css({'color':'#86EFCC'});
+            $('nav a').hover(function(){
+              $(this).css({'color':'white'});
+            },function(){
+              $(this).css({'color':'#86EFCC'});
+            });
+            flag = false;
+          }
+        }
+      });
+
+      $('nav a').click(function(){
+        if($(this).attr('id')=='nosotros'){
+          $('html, body').animate({
+            scrollTop: $("#part1").offset().top-100
+          }, 2000);
+        }else if($(this).attr('id')=='servicios'){
+          $('html, body').animate({
+            scrollTop: $("#part2A").offset().top-25
+          }, 2000);
+        }else if($(this).attr('id')=='tienda'){
+          $('html, body').animate({
+            scrollTop: $("#part3A").offset().top-25
+          }, 2000);
+        }else if($(this).attr('id')=='contacto'){
+          $('html, body').animate({
+            scrollTop: $("#part4A").offset().top-25
+          }, 2000);
+        }
+      });
+
+      $("#logo").click(function() {
+        $('html, body').animate({
+          scrollTop: 0
+        }, 2000);
+      });
+
+      $(window).scroll(function(){
+        if($(window).scrollTop()>$("#part2A").offset().top-25){
+            $("#part1 h1").removeClass('wow fadeInUp');
+            $("#part1 p").removeClass('wow fadeInUp');
+            $("#part1 img").removeClass('wow fadeInUp');
+            if($(window).scrollTop()>$("#part3A").offset().top-25){
+              $(".servicio").removeClass('wow fadeInUp');
+              if($(window).scrollTop()>$("#part4A").offset().top-25){
+                $(".contenido").removeClass('wow fadeInUp');
+              }
+            }
+          }
+      });
+
+    });
---
+++ php/envio.php
@@ -0,0 +1,86 @@
+<?php
+
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+//Create a new PHPMailer instance
+$mail = new PHPMailer;
+$mail->SMTPOptions = array(
+    'ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true
+    )
+);
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
+//Enable SMTP debugging
+// 0 = off (for production use)
+// 1 = client messages
+// 2 = client and server messages
+$mail->SMTPDebug = 4;
+//Set the hostname of the mail server
+$mail->Host = 'smtp.gmail.com';
+// use
+// $mail->Host = gethostbyname('smtp.gmail.com');
+// if your network does not support SMTP over IPv6
+//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+$mail->Port = 587;
+//Set the encryption system to use - ssl (deprecated) or tls
+$mail->SMTPSecure = 'tls';
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+//Username to use for SMTP authentication - use full email address for gmail
+$mail->Username = "pruebaemail13@gmail.com";
+//Password to use for SMTP authentication
+$mail->Password = "markos_13";
+//Set who the message is to be sent from
+$mail->setFrom($_POST['email'], $_POST['nombre']);
+//Set an alternative reply-to address
+// $mail->addReplyTo('replyto@example.com', 'First Last');
+//Set who the message is to be sent to
+$mail->addAddress('pruebaemail13@gmail.com', 'PruebaEmail');
+//Set the subject line
+$mail->Subject = $_POST['asunto'];
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+$mail->msgHTML("<p>".$_POST['mensaje']."</p>", __DIR__);
+//Replace the plain text body with one created manually
+$mail->AltBody = $_POST['mensaje'];
+//Attach an image file
+// $mail->addAttachment('images/phpmailer_mini.png');
+//send the message, check for errors
+if (!$mail->send()) {
+    echo "<script>alert('El mensaje no se ha podido enviar.');</script>";
+    headers('Location:"http://localhost/pepe-project/html"');
+} else {
+    echo "<script>alert('El mensaje se mando correctamente.');</script>";
+      headers('Location:"http://localhost/pepe-project/html"');
+    //Section 2: IMAP
+    //Uncomment these to save your message in the 'Sent Mail' folder.
+    #if (save_mail($mail)) {
+    #    echo "Message saved!";
+    #}
+}
+//Section 2: IMAP
+//IMAP commands requires the PHP IMAP Extension, found at: https://php.net/manual/en/imap.setup.php
+//Function to call which uses the PHP imap_*() functions to save messages: https://php.net/manual/en/book.imap.php
+//You can use imap_getmailboxes($imapStream, '/imap/ssl') to get a list of available folders or labels, this can
+//be useful if you are trying to get this working on a non-Gmail IMAP server.
+function save_mail($mail)
+{
+    //You can change 'Sent Mail' to any other folder or tag
+    $path = "{imap.gmail.com:993/imap/ssl}[Gmail]/Sent Mail";
+    //Tell your server to open an IMAP connection using the same username and password as you used for SMTP
+    $imapStream = imap_open($path, $mail->Username, $mail->Password);
+    $result = imap_append($imapStream, $path, $mail->getSentMIMEMessage());
+    imap_close($imapStream);
+    return $result;
+}
+ ?>
