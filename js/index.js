156aff9fb83d51d68cbb24f24e38b010539383b9
MarcosTorreiro <MarcosTorreiro@users.noreply.github.com>
2017-11-04 22:16:57 +0100

    Some Changes

--- js/index.js
+++ js/index.js
@@ -22,13 +22,7 @@
         }else{
           if(flag){
             var tamaño = $(window).width();
-            if(tamaño > 1450){
-              $('#logo').css({'margin-top':'150px','width':'250px','height':'250px'});
-            }else if(tamaño > 750){
-              $('#logo').css({'margin-top':'150px','width':'175px','height':'175px'});
-            }else if(tamaño <= 750){
-              $('#logo').css({'margin-top':'150px','width':'125px','height':'125px'});
-            }
+            $('#logo').css({'margin-top':'150px','width':'250px','height':'250px'});
             $('header').css({'background-color':'transparent', 'color':'yellow'});
             $('nav a').css({'color':'#86EFCC'});
             $('nav a').hover(function(){
@@ -67,6 +61,36 @@
         }, 2000);
       });
 
+      $("#inicio").click(function() {
+        $('html, body').animate({
+          scrollTop: 0
+        }, 2000);
+      });
+
+      $("#about").click(function() {
+        $('html, body').animate({
+          scrollTop: $("#part1").offset().top-100
+        }, 2000);
+      });
+
+      $("#services").click(function() {
+        $('html, body').animate({
+          scrollTop: $("#part2A").offset().top-25
+        }, 2000);
+      });
+
+      $("#shop").click(function() {
+        $('html, body').animate({
+          scrollTop: $("#part3A").offset().top-25
+        }, 2000);
+      });
+
+      $("#contact").click(function() {
+        $('html, body').animate({
+          scrollTop: $("#part4A").offset().top-25
+        }, 2000);
+      });
+
       $(window).scroll(function(){
         if($(window).scrollTop()>$("#part2A").offset().top-25){
             $("#part1 h1").removeClass('wow fadeInUp');
@@ -86,10 +110,8 @@
           if($(window).scrollTop()<201){
           if(tamaño > 1450){
             $('#logo').css({'width':'250px','height':'250px'});
-          }else if(tamaño > 750){
+          }else if(tamaño < 736){
             $('#logo').css({'width':'175px','height':'175px'});
-          }else {
-            $('#logo').css({'width':'125px','height':'125px'});
           }
         }
         });
